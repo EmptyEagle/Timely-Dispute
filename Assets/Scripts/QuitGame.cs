b015102ead803069923f07e6d5c0cf3ba21ee99b
EmptyEagle <owen@littlegilbert.com>
2024-09-29 15:47:18 -0400

    Update QuitGame.cs

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -6,6 +6,12 @@ public class QuitGame : MonoBehaviour
 {
     public KeyCode quitButton = KeyCode.Escape;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
